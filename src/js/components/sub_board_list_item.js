47654cdc8b3806c30bc10565c5e03f9385a287fd
Duleja <lazic_dusan@yahoo.co.uk>
2016-09-30 10:04:54 +0200

    Added main.scss and some styles with responsive included in the src.index.html head tag

--- src/js/components/sub_board_list_item.js
+++ src/js/components/sub_board_list_item.js
@@ -7,8 +7,16 @@ export default class SubBoardListItem extends React.Component {
     return (
       <div className="list-group">
         <Link to={`/category/${sub_board.url}/topics`} className="list-group-item active">
-          <h4 className="list-group-item-heading">{sub_board.name}</h4>
-          <p className="list-group-item-text">{sub_board.description}</p>
+          <div className="sub-board">
+            <div className="sub-board-name-and-description">
+              <h4 className="list-group-item-heading">{sub_board.name}</h4>
+              <p className="list-group-item-text">{sub_board.description}</p>
+            </div>
+            <div className="sub-board-last-post">
+              <p className="sub-board-last-post-title"> Last post: </p>
+              <p className="sub-board-last-post-user">{sub_board.post}</p>
+            </div>
+          </div>
         </Link>
       </div>
     );
